ef0a659608d3ee0dc92fb40ce9f6f6307056e764
Autobahntunnelfisch <cankinik@web.de>
2025-03-24 14:33:00 +0100

    feat: create StreamBuilder

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
@@ -7,29 +8,44 @@ class GoldScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     /// Platzhalter für den Goldpreis
     /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
+    final goldPrices = getGoldPriceStream();
 
     return SafeArea(
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               _buildHeader(context),
               const SizedBox(height: 20),
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
+              StreamBuilder<double>(
+                  stream: goldPrices,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData &&
+                            snapshot.connectionState == ConnectionState.done ||
+                        snapshot.connectionState == ConnectionState.active) {
+                      return Text(
+                        NumberFormat.simpleCurrency(locale: 'de_DE')
+                            .format(snapshot.data),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.primary),
+                      );
+                    } else if (snapshot.connectionState !=
+                        ConnectionState.done) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      return const Text("Fehlerhafte Daten");
+                    }
+                  }),
               // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
               // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
-              ),
             ],
           ),
         ),
@@ -39,6 +55,7 @@ class GoldScreen extends StatelessWidget {
 
   Row _buildHeader(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Image(
           image: AssetImage('assets/bars.png'),
